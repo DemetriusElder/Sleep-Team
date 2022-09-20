b64194ea68dbf3079ce5c9139c73148548e98e3a
Lewis <2221744@cognizant.com>
2022-09-20 14:40:17 -0500

    game settings, scoreboard more organized

--- angular/src/app/game-settings/game-settings.component.ts
+++ angular/src/app/game-settings/game-settings.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ScoreboardComponent } from '../scoreboard/scoreboard.component';
 
 @Component({
   selector: 'app-game-settings',
@@ -7,17 +8,20 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./game-settings.component.css']
 })
 export class GameSettingsComponent implements OnInit {
+  player!: string;
   
   settingsForm = new FormGroup({
-    playerName: new FormControl('', [
-      Validators.required,
-      Validators.minLength(2),
-    ]),
-   
+    char: new FormControl('', [Validators.required]),
+    difficulty: new FormControl('', [Validators.required])
   });
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log(this.settingsForm.value.char);
+    console.log(this.settingsForm.value.difficulty);
   }
 
 }
