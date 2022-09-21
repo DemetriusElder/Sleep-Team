78f3f01703b68ca97e0f4f26f07d56c6d5801280
Kuang <2221741@cognizant.com>
2022-09-21 09:46:29 -0400

    updated table users naming, and add some dummy statements

--- TicTacToe/src/main/java/SleepTeam/TicTacToe/Model/User.java
+++ TicTacToe/src/main/java/SleepTeam/TicTacToe/Model/User.java
@@ -7,6 +7,7 @@ import javax.persistence.Table;
 
 
 @Entity
+@Table(name = "users")
 public class User {
 	
 	@Id
@@ -20,7 +21,7 @@ public class User {
 	private int wins;
 	
 	private int losses;
-	
+
 	public User() {
 		
 	}
